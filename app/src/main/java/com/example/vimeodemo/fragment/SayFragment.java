e1fc00ee43133f11ebd360cfaa90f20cfd4ac50a
小七七 <2247697783@qq.com>
2017-08-14 16:16:17 +0800

    第一次提交

---
+++ app/src/main/java/com/example/vimeodemo/fragment/SayFragment.java
@@ -0,0 +1,27 @@
+package com.example.vimeodemo.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vimeodemo.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SayFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_say, null);
+        return view;
+    }
+
+}
